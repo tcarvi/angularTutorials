5d44d90bb13c6f4b500e8743a2642179655c35b4
Eduardo dos Santos Leal <eduardoleal1981@gmail.com>
2023-03-24 23:57:40 -0300

    Update

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/eduardosantosleal/src/github.com/tcarvi/angularTutorials/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/eduardosantosleal/src/github.com/tcarvi/angularTutorials/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/eduardosantosleal/src/github.com/tcarvi/angularTutorials/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/eduardosantosleal/src/github.com/tcarvi/angularTutorials/src/css/custom.css'),
-];
